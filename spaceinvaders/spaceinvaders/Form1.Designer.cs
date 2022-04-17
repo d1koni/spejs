7abf315ab8886c325a8a5853656058cf08653534
d1koni <83756361+d1koni@users.noreply.github.com>
2022-04-17 09:41:07 +0200

    Add files via upload
    za pocetak

---
+++ spaceinvaders/spaceinvaders/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace spaceinvaders
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.timer4 = new System.Windows.Forms.Timer(this.components);
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.avion2 = new System.Windows.Forms.PictureBox();
+            this.avion = new System.Windows.Forms.PictureBox();
+            this.timer5 = new System.Windows.Forms.Timer(this.components);
+            this.timer6 = new System.Windows.Forms.Timer(this.components);
+            this.timer7 = new System.Windows.Forms.Timer(this.components);
+            this.timer8 = new System.Windows.Forms.Timer(this.components);
+            this.gametimer = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.MetakTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.avion2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.avion)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 10;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // timer3
+            // 
+            this.timer3.Interval = 10;
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // timer4
+            // 
+            this.timer4.Interval = 10;
+            this.timer4.Tick += new System.EventHandler(this.timer4_Tick);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // avion2
+            // 
+            this.avion2.BackColor = System.Drawing.Color.Black;
+            this.avion2.Image = global::spaceinvaders.Properties.Resources.avion2;
+            this.avion2.Location = new System.Drawing.Point(240, 559);
+            this.avion2.Name = "avion2";
+            this.avion2.Size = new System.Drawing.Size(78, 75);
+            this.avion2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.avion2.TabIndex = 1;
+            this.avion2.TabStop = false;
+            this.avion2.Tag = "player";
+            this.avion2.Visible = false;
+            // 
+            // avion
+            // 
+            this.avion.BackColor = System.Drawing.Color.Black;
+            this.avion.Image = global::spaceinvaders.Properties.Resources.avion1;
+            this.avion.Location = new System.Drawing.Point(572, 559);
+            this.avion.Name = "avion";
+            this.avion.Size = new System.Drawing.Size(78, 75);
+            this.avion.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.avion.TabIndex = 0;
+            this.avion.TabStop = false;
+            this.avion.Tag = "player";
+            this.avion.Visible = false;
+            // 
+            // timer5
+            // 
+            this.timer5.Interval = 10;
+            this.timer5.Tick += new System.EventHandler(this.timer5_Tick);
+            // 
+            // timer6
+            // 
+            this.timer6.Interval = 10;
+            this.timer6.Tick += new System.EventHandler(this.timer6_Tick);
+            // 
+            // timer7
+            // 
+            this.timer7.Interval = 10;
+            this.timer7.Tick += new System.EventHandler(this.timer7_Tick);
+            // 
+            // timer8
+            // 
+            this.timer8.Interval = 10;
+            this.timer8.Tick += new System.EventHandler(this.timer8_Tick);
+            // 
+            // gametimer
+            // 
+            this.gametimer.Enabled = true;
+            this.gametimer.Interval = 1;
+            this.gametimer.Tick += new System.EventHandler(this.gametimer_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(122, 131);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(626, 108);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "GAME OVER";
+            this.label1.Visible = false;
+            // 
+            // MetakTimer
+            // 
+            this.MetakTimer.Interval = 7;
+            this.MetakTimer.Tick += new System.EventHandler(this.MetakTimer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(12)))));
+            this.ClientSize = new System.Drawing.Size(884, 718);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.avion2);
+            this.Controls.Add(this.avion);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = " ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.avion2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.avion)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox avion;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Timer timer4;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox avion2;
+        private System.Windows.Forms.Timer timer5;
+        private System.Windows.Forms.Timer timer6;
+        private System.Windows.Forms.Timer timer7;
+        private System.Windows.Forms.Timer timer8;
+        private System.Windows.Forms.Timer gametimer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer MetakTimer;
+    }
+}
+
---
+++ spaceinvaders/spaceinvaders/Form1.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace spaceinvaders
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int br = 0;
+        PictureBox[] metak;
+        Image metkovi = Image.FromFile(@"C:\Users\kurs\Documents\Visual Studio 2017\Projects\spaceinvaders\slike\metak.png");
+        
+        public int pspeed = 10;
+        
+        public void GameOver()
+        {
+            label1.Visible = true;
+            pspeed = 0;
+            gametimer.Stop();
+        }
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+            avion.Visible = true;
+            avion2.Visible = true;
+            int metaksp = 20;
+            metak = new PictureBox[5];
+            
+            
+            
+
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up)
+            {
+                timer1.Start();
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                timer2.Start();
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                timer3.Start();
+            }
+            if (e.KeyCode == Keys.Left)
+            {
+                timer4.Start();
+            }
+            if (e.KeyCode == Keys.W)
+            {
+                timer5.Start();
+            }
+            if (e.KeyCode == Keys.S)
+            {
+                timer6.Start();
+            }
+            if (e.KeyCode == Keys.D)
+            {
+                timer7.Start();
+            }
+            if (e.KeyCode == Keys.A)
+            {
+                timer8.Start();
+            }
+            
+
+        }
+
+        
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up)
+            {
+                timer1.Stop();
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                timer2.Stop();
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                timer3.Stop();
+            }
+            if (e.KeyCode == Keys.Left)
+            {
+                timer4.Stop();
+            }
+            if (e.KeyCode == Keys.W)
+            {
+                timer5.Stop();
+            }
+            if (e.KeyCode == Keys.S)
+            {
+                timer6.Stop();
+            }
+            if (e.KeyCode == Keys.D)
+            {
+                timer7.Stop();
+            }
+            if (e.KeyCode == Keys.A)
+            {
+                timer8.Stop();
+            }
+            if (e.KeyCode == Keys.Space)
+            {
+                if(br < 5)
+                {
+                    metak[br] = new PictureBox();
+                    metak[br].Size = new Size(8, 10);
+                    metak[br].Image = metkovi;
+                    metak[br].SizeMode = PictureBoxSizeMode.StretchImage;
+                    metak[br].Location = new Point( avion2.Location.X + 39, avion2.Location.Y);
+                    this.Controls.Add(metak[br]);
+                    foreach(Control x in this.Controls)
+                    {
+                        if(x is PictureBox)
+                        {
+                            x.Tag = "bullet";
+                        }
+                    }
+                    br++;
+                }
+
+            }
+            if(e.KeyCode == Keys.Enter)
+            {
+
+            }
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            if(avion.Top < 625)
+            {
+                avion.Top += pspeed;
+            }
+        }
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            if(avion.Left < 800)
+            {
+                avion.Left += pspeed;
+            }
+        }
+
+        private void timer4_Tick(object sender, EventArgs e)
+        {
+            if (avion.Left > 13)
+            {
+                avion.Left -= pspeed;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (avion.Top > 100)
+            {
+                avion.Top -= pspeed;
+            }
+        }
+
+        private void timer5_Tick(object sender, EventArgs e)
+        {
+            if (avion2.Top > 100)
+            {
+                avion2.Top -= pspeed;
+            }
+        }
+
+        private void timer6_Tick(object sender, EventArgs e)
+        {
+            if (avion2.Top < 625)
+            {
+                avion2.Top += pspeed;
+            }
+        }
+
+        private void timer7_Tick(object sender, EventArgs e)
+        {
+            if (avion2.Left < 800)
+            {
+                avion2.Left += pspeed;
+            }
+        }
+
+        private void timer8_Tick(object sender, EventArgs e)
+        {
+            if (avion2.Left > 13)
+            {
+                avion2.Left -= pspeed;
+            }
+        }
+
+        private void gametimer_Tick(object sender, EventArgs e)
+        {
+            if (avion.Bounds.IntersectsWith(avion2.Bounds))
+            {
+                GameOver();
+            }
+                
+        }
+
+        private void MetakTimer_Tick(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
